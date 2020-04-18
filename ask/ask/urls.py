050e78d42fe9439515b6bc9eebeb622ff6a639b8
voloodya <Vlad-i-mir.rus@yandex.ru>
2020-04-18 15:07:50 +0000

    First ask

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	url(r'^$', include('qa.urls')),
+	#path(r'^admin/', admin.site.urls),
+	url(r'^question/$', include('qa.urls')),
+	#url(r'^$', not_found),
 ]
