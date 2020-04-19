3d83c54a5a0a159eb2425f0d0c0b5392f45d2cf5
voloodya <Vlad-i-mir.rus@yandex.ru>
2020-04-19 09:27:45 +0000

    First ask

--- ask/startGunicom.sh
+++ ask/startGunicom.sh
@@ -1,1 +1,5 @@
+echo "Запуск gunicorn:"
 sudo gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application &
+#sudo gunicorn --bind='0.0.0.0:8000' ask.wsgi
+
+echo "Завершение запуска gunicorn"
