95f3c26ab9fd7b27622af8e472c7b003682997cc
voloodya <Vlad-i-mir.rus@yandex.ru>
2020-04-19 10:42:30 +0000

    First ask

--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
 #sudo ln -sf  /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo rm /etc/nginx/sites-enabled/default
-sudo mv /home/box/web/etc/default.txt /home/box/web/etc/default
-sudo ln -sf /home/box/web/etc/default /etc/nginx/sites-enabled/default
+#sudo mv /home/box/web/etc/default.txt /home/box/web/etc/default
+#sudo ln -sf /home/box/web/etc/default /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
+sudo mv /home/box/web/etc/nginx.conf /home/box/web/etc/default
+echo "/ETC/:  "
+ls /home/box/web/etc/
+echo "Фаил default:  "
+cat /home/box/web/etc/default
+echo "Перезапись символ ссылки:  "
+sudo ln -sf /home/box/web/etc/default /etc/nginx/sites-enabled/default
+echo "Содержимое '/etc/nginx/sites-enabled/default':  "
+cat /etc/nginx/sites-enabled/default
+echo "Перезапуск NGINX:  "
 sudo /etc/init.d/nginx restart
+
 #sudo ls /etc/nginx/sites-enabled/
 #sudo gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
 #sudo gunicorn --bind='0.0.0.0:8000' ask.wsgi 
